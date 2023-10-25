7b55aebf1e62ac67e7b9acabcd472d9e6f78aab9
DaNGoiz <3332099165@qq.com>
2023-10-25 14:26:42 +0800

    Update map item generation

--- GenerateMap.cpp
+++ GenerateMap.cpp
@@ -8,6 +8,8 @@ const int mapY = 100;
 
 using CharArray2D = char[25][100];
 
+bool needToChange[25][100] = {}; // hardcode
+
 // initialize map
 void initializeMap(CharArray2D& map, char c) {
     for (int i = 0; i < mapX; ++i)
@@ -70,78 +72,66 @@ void cleanAndGetBlankSpace(CharArray2D& map, int blankSpace[mapX][mapY], int& nu
 }
 
 // prove the roads are connected
-bool provePathIsConnected(CharArray2D& map) {
-    bool isConnected = false; 
-    
-    isConnected = true; // deubg
 
-    return isConnected;
+// define four direction movement
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, 1, -1};
+
+// check the position is inside map
+bool isValid(int x, int y) {
+    return x >= 0 && x < mapX && y >= 0 && y < mapY;
 }
 
-// // define four direction movement
-// int dx[] = {1, -1, 0, 0};
-// int dy[] = {0, 0, 1, -1};
+// go through all blank spaces
+void dfs(int x, int y, CharArray2D map, bool visited[mapX][mapY]) {
+    visited[x][y] = true;
+    for (int i = 0; i < 4; i++) {
+        int newX = x + dx[i];
+        int newY = y + dy[i];
+        if (isValid(newX, newY) && map[newX][newY] == ' ' && !visited[newX][newY]) {
+            dfs(newX, newY, map, visited);
+        }
+    }
+}
 
-// // check the position is inside map
-// bool isValid(int x, int y) {
-//     return x >= 0 && x < mapX && y >= 0 && y < mapY;
-// }
+// check is blank spaces connected
+bool isBlankSpaceConnected(CharArray2D& map) {
 
-// // DFS go through all blank space
-// void dfs(int x, int y, CharArray2D map, bool visited[mapX][mapY]) {
-//     visited[x][y] = true;
-//     for (int i = 0; i < 4; i++) {
-//         int newX = x + dx[i];
-//         int newY = y + dy[i];
-//         if (isValid(newX, newY) && map[newX][newY] == ' ' && !visited[newX][newY]) {
-//             dfs(newX, newY, map, visited);
-//         }
-//     }
-// }
+    bool visited[mapX][mapY];
+    for (int i = 0; i < mapX; i++) {
+        for (int j = 0; j < mapY; j++) {
+            visited[i][j] = false;
+        }
+    }
 
-// // check is blank space connected
-// bool isBlankSpaceConnected(CharArray2D& map) {
+    // find the first blank place to dfs
+    int startX, startY;
+    for (int i = 0; i < mapX; i++) {
+        for (int j = 0; j < mapY; j++) {
+            if (map[i][j] == ' ') {
+                startX = i;
+                startY = j;
+                break;
+            }
+        }
+    }
 
-//     cout << "enter function";
+    dfs(startX, startY, map, visited);
 
-//     bool visited[mapX][mapY];
-//     // initialize vizited array
-//     for (int i = 0; i < mapX; i++) {
-//         for (int j = 0; j < mapY; j++) {
-//             visited[i][j] = false;
-//         }
-//     }
+    // check if all blank spaces can be visited
+    for (int i = 0; i < mapX; i++) {
+        for (int j = 0; j < mapY; j++) {
+            if (map[i][j] == ' ' && !visited[i][j]) {
+                return false;
+            }
+        }
+    }
 
-//     // find the first blank place to dfs
-//     int startX, startY;
-//     for (int i = 0; i < mapX; i++) {
-//         for (int j = 0; j < mapY; j++) {
-//             if (map[i][j] == ' ') {
-//                 startX = i;
-//                 startY = j;
-//                 break;
-//             }
-//         }
-//     }
+    return true;
+}
 
-//     dfs(startX, startY, map, visited);
-
-//     // check if all blank spaces can be visited
-//     for (int i = 0; i < mapX; i++) {
-//         for (int j = 0; j < mapY; j++) {
-//             if (map[i][j] == ' ' && !visited[i][j]) {
-//                 return false;
-//             }
-//         }
-//     }
-
-//     return true;
-// }
-
-// turn the outside walls to X
-void generateWalls(CharArray2D& map) {
-    bool needToChange[25][100] = {}; // hardcode
-
+// turn the outside walls to char 'X'
+void generateWalls(CharArray2D& map) { 
     for (int i = 0; i < mapX; ++i)
         for (int j = 0; j < mapY; ++j)
             if (map[i][j] == '-')
@@ -224,17 +214,22 @@ void generateMapElements(CharArray2D& map, int blankSpace[mapX][mapY]) {
 void generateItems(CharArray2D& map, int blankSpace[mapX][mapY]) {
 
     int propTotal = 500;
-    int propMax = 3;
-
-    const char items[] = {'$', '+', '^', '='}; // gold, health, spike, shop
+    int propMax = 7;
 
     for (int i = 0; i < mapX; ++i) {
         for (int j = 0; j < mapY; ++j) {
             if (blankSpace[i][j] == 1) {
                 if (rand() % propTotal <= propMax)
                 {
-                    int itemIndex = rand() % 4; // 4 items
-                    map[i][j] = items[itemIndex];
+                    int itemIndex = rand() % 22;
+                    if (itemIndex < 7)
+                        map[i][j] = '$';
+                    else if (itemIndex >= 7 && itemIndex < 10)
+                        map[i][j] = '+';
+                    else if (itemIndex >= 10 && itemIndex < 13)
+                        map[i][j] = '=';
+                    else 
+                        map[i][j] = '^';
                 }
             }
         }
@@ -243,6 +238,8 @@ void generateItems(CharArray2D& map, int blankSpace[mapX][mapY]) {
 
 // generate a random map
 void generateRandomMap(CharArray2D& map) {
+    srand(time(NULL));
+
     //generate blank map
     int moveableSpace = 0;
     int blankSpaceArr[25][100];
@@ -253,8 +250,7 @@ void generateRandomMap(CharArray2D& map) {
         initializeMap(map, '-');
         generateBlankSpace(map);
         cleanAndGetBlankSpace(map, blankSpaceArr, moveableSpace);
-        // isConnected = isBlankSpaceConnected(map);
-        isConnected = provePathIsConnected(map);
+        isConnected = isBlankSpaceConnected(map);
     }
         
     // generate terrain
@@ -269,19 +265,19 @@ void generateRandomMap(CharArray2D& map) {
 // set color and print map elements
 void printMapElement(char element) {
     if (element == '@')
-        cout << "\x1b[43m" << element << "\x1b[0m";
+        cout << "\x1b[43;30m" << element << "\x1b[0m";
     else if (element == '$' || element == '=')
         cout << "\x1b[33m" << element << "\x1b[0m";
     else if (element == '\'' || element == '"' || element == '`')
         cout << "\x1b[32m" << element << "\x1b[0m";
     else if (element == '#' || element == '!')
-        cout << "\x1b[46m" << element << "\x1b[0m";
+        cout << "\x1b[46;30m" << element << "\x1b[0m";
     else if (element == 'X')
         cout << "\x1b[90m" << element << "\x1b[0m";
     else if (element == '+')
         cout << "\x1b[31m" << element << "\x1b[0m";
-    else if (element == '^')
-        cout << "\x1b[90m" << element << "\x1b[0m";
+    else if (element == '^') // different color
+        cout << "\x1b[31m" << element << "\x1b[0m";
     else
         cout << element;
 }
@@ -297,9 +293,7 @@ void printMap(const CharArray2D& map) {
 }
 
 int main() {
-    srand(time(NULL));
-    
-    CharArray2D map = {};
+    CharArray2D map = {}; // in outside classes also use
 
     generateRandomMap(map);
     printMap(map);
