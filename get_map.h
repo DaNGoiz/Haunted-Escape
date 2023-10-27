c832d334c1e68a41d311147c51a4e2cfab373a20
Izzy <3332099165@qq.com>
2023-10-27 06:58:22 +0000

    Add commit to headers

--- get_map.h
+++ get_map.h
@@ -1,7 +1,10 @@
 #ifndef GET_MAP_H
 #define GET_MAP_H
 
+// Read all data from map.log
 void getMapDataAll(char map[25][100]);
+
+// Read data at point (x, y) from map.log
 char getMapDataAtPoint(int x, int y);
 
 #endif
--- set_map.h
+++ set_map.h
@@ -1,9 +1,16 @@
 #ifndef SET_MAP_H
 #define SET_MAP_H
 
+// Set all data in map.log
 void setMapDataAll(char map[25][100]);
+
+// Set data at point (x, y) in map.log
 void setMapDataAtPoint(int x, int y, char c, char map[25][100]);
+
+// Generate new map and set it in map.log
 void newMap(char map[25][100]);
+
+// Clear map in map.log
 void clearMap(char map[25][100]);
 
 #endif
