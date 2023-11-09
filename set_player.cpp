3cc13bbaef7a54389d62f41547822f373ac9e3db
MickyZhouAAA <146114365+MickyZhouAAA@users.noreply.github.com>
2023-11-09 20:33:40 +0800

    change the player's data

---
+++ set_player.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+//Player's structure
+struct Player{
+    int Health;
+    int Shield;
+    int Gold;
+    int x,y;
+    bool Key;
+    int Level;
+};
+
+//Give the player initial data
+Player p;
+Player p ={5,0,0,50,25,false,1};
+
+//Initialize the level of game
+int Level=1;
+
+//Operations on player's data
+void ChangeHealth(int change){
+    p.Health=p.Health+change;
+
+}
+
+void ChangeShield(int change){
+    p.Shield=p.Shield+change;
+}
+
+void ChangeGold(int change){
+    p.Gold=p.Gold+change;
+}
+
+void ChangeKey(){
+    p.Key=!p.Key;
+}
+
+void SetLevel(int level){
+    Level=level;
+}
+
+void NewPlayer(){
+    p ={5,0,0,50,25,false,1};
+}
+
+int main(){
+    //commend towards the change of setting
+    return 0;
+}
