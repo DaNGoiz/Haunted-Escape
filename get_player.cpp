827e5fdc78ab1db6a08fc272598155a141f1630d
MickyZhouAAA <146114365+MickyZhouAAA@users.noreply.github.com>
2023-11-09 20:32:58 +0800

    get_player.cpp
    initialize the player's data and print the data on Player.log

--- get_player.cpp
+++ get_player.cpp
@@ -6,8 +6,44 @@
 #include <cstdlib>
 using namespace std;
 
-//get all kinds of player data from Player.log
 
+//initialize the player at the begining
+struct Player{
+    int Health;
+    int Shield;
+    int Gold;
+    int x,y;
+    bool Key;
+    int Level;
+};
+Player p;
+Player p ={5,0,0,50,25,false,1};
+
+
+
+
+void printplayer(){
+    ofstream fout("Player.log");
+    if(fout.fail()){
+        cout<<"Error in file opening"<<endl;
+        exit(1);
+    }
+    fout<<"Health: "<<p.Health<<"\n";
+    fout<<"Shield: "<<p.Shield<<"\n";
+    fout<<"Gold: "<<p.Gold<<"\n";
+    fout<<"Position: "<<p.x<<","<<p.y<<"\n";
+    fout<<"Key: "<<p.Key<<"\n";
+    fout<<"Level: "<<p.Level<<endl;
+    fout.close();
+}
+    
+
+
+
+
+
+
+//get all kinds of player data from Player.log
 int main(){
     ifstream fin("Player.log");
     //prevent opening error
@@ -15,6 +51,7 @@ int main(){
         cout<<"Error in file opening"<<endl;
         exit(1);
     }
+
 
     if(!fin.is_open()){
         return 0;
