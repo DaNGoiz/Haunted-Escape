eb7d7445c82fe3f47f91d84fea1dd5dfd116700c
MickyZhouAAA <146114365+MickyZhouAAA@users.noreply.github.com>
2023-11-12 14:47:08 +0800

    listing all the goods in the shop in Shop.log

---
+++ generrate_shop.h
@@ -0,0 +1,13 @@
+#ifndef GENSHOP_H
+#define GENSHOP_H
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <cstdlib>
+
+void printgood(g1);
+
+#endif
