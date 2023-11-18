0d9a4af728788ba9b2231410c3185e50dd4687fd
Wang Yifu <145696164+Wang-YI-fu@users.noreply.github.com>
2023-11-18 22:27:22 +0800

    ADD print_victory_menu.cpp

---
+++ print_victory_menu.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+char printVictoryMenu(int high,int length){
+        int high = 15;
+        int length = 45;
+    for (int i = 0; i<=high; i++){
+        for(int j=0;j<=length;j++){
+            if (i==0 && j==0){
+                    cout<<"┏";
+            }
+            else if(i==0 && j==length){
+                    cout<<"┓";
+            }
+            else if(i==high && j==0){
+                    cout<<"┗";
+            }
+            else if(i==high && j==length){
+                    cout<<"┛";
+            }
+            else if (j==0 || j==length){
+                    cout << "┃";
+            }
+            else if (i==0||i==high){
+                    cout<<"━";
+
+            }
+            else if (i==7 && j==18){
+                    cout<<"V";
+            }
+            else if (i==7 && j==19){
+                    cout<<"I";
+            }
+            else if (i==7 && j==20){
+                    cout<<"C";
+            }
+            else if (i==7 && j==21){
+                    cout<<"T";
+            }
+            else if (i==7 && j==22){
+                    cout<<"O";
+            }
+            else if (i==7 && j==23){
+                    cout<<"R";
+            }
+            else if (i==7 && j==24){
+                    cout<<"Y";
+            }
+            else if (i==7 && j==25){
+                    cout<<"!";
+            }
+            
+            else{
+                    cout<<" ";
+            }
+        }
+        cout<<endl;
+    
+    }
+    return ' ';
+}
+
