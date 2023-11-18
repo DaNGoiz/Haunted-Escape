26ca5a109f8021a671dae96eb788531d2722f79e
xujingxiang19883 <122252536+xujingxiang19883@users.noreply.github.com>
2023-11-18 16:29:22 +0800

    complete #include and add some comments

--- GameStatus.cpp
+++ GameStatus.cpp
@@ -1,32 +1,44 @@
-// #include "InGamePlayerMove.h"
+#include "InGamePlayerMove.h"
+#include "InShopPlayerMove.h"
 #include <iostream>
 #include <string>
 #include "get_map.h"
 #include "set_map.h"
+#include "print_end_menu.h"
+#include "print_start_menu.h"
+#include "print_victory_menu.h"
+#include "timer.h"
 using namespace std;
+// This is the only main program throughout the project.
+// This program controls GAME STATUS. Make sure it is always one of the following: Start; InGame; Pause; LoadLevel; GameOver; Shop; Victory.
 int main(){
     string status = "Start";
     string input;
     bool gameOn = true;
+    // a while loop makes sure that the game is running unless interrupted by changing gameOn = false.
     while (gameOn){
         string input;
         cin >> input;
         if (status == "Start"){
             printStartMenu();
+            // player starts, timer starts, game starts
             if (input == "s"){
                 printGameMenu();
                 Timer(true);
                 status = "InGame";
             }
+            // player chooses to end the game
             if (input == "e"){
                 Timer(false);
                 status = "GameOver";
             }
         }
+            // In Game, we use InGamePlayerMove() to interpret the move of the user.
         else if (status == "InGame"){
             Timer(true);
             status = InGamePlayerMove();
         }
+            // Pause: if enter "c", then continue game, else enter "r" to restart the game.
         else if (status == "Pause"){
             Timer(false);
             if (input == "c"){
@@ -39,22 +51,26 @@ int main(){
             status = "Start";
             }
         }
+            // when change to new level, generate a new map.
         else if (status == "LoadLevel"){
             Timer(false);
             char map[25][100];
             newMap(map);
             status = "InGame";
         }
+            // go to shop, use InShopPlayerMove to interpret the move of the user.
         else if (status == "Shop"){
             Timer(false);
             printShopMenu();
             status = InShopPlayerMove();
         }
+            // Gameover. Game stops. While loop stops.
         else if (status == "GameOver"){
             Timer(false);
             printEndMenu();
             gameOn = false;
         }
+            // Victory. Game stops. While loop stops.
         else if (status == "Victory"){
             Timer(false);
             printVictoryMenu();
