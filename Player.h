fd4fbd24b88d959b4734c0197a90bdf8e128d6e3
MickyZhouAAA <146114365+MickyZhouAAA@users.noreply.github.com>
2023-11-23 22:19:18 +0800

    Updated**Player.h

--- Player.h
+++ Player.h
@@ -1,3 +1,6 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
 // Initialize the player at the begining
 struct Player {
     int Health;
@@ -7,3 +10,19 @@ struct Player {
     bool Key;
     int Level;
 };
+Player GetPlayer();
+int GetHealth();
+int GetShield();
+int GetGold();
+int* GetPosition();
+bool GetKey();
+int GetLevel();
+void ChangeHealth(int change);
+void ChangeShield(int change);
+void ChangeGold(int change);
+void ChangePosition(int x, int y);
+void ChangeKey(bool hasKey);
+void ChangeLevel(int change);
+void SetLevel(int level);
+void NewPlayer();
+#endif
