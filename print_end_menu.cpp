5f24a6cc2443eb5acc0ed7c2864776b08850d931
Wang-YI-fu <u3609815@connect.hku.hk>
2023-11-26 20:41:01 +0800

    draw the start, gameover ,and victory

--- print_end_menu.cpp
+++ print_end_menu.cpp
@@ -1,59 +1,25 @@
 #include <iostream>
+
 using namespace std;
 
-void printEndMenu(int high,int length){
-    high = 15;
-    length = 45;   
-    for (int i = 0; i<=high; i++){
-        for(int j=0;j<=length;j++){
-            if (i==0 && j==0){
-                    cout<<"┏";
-            }
-            else if(i==0 && j==length){
-                    cout<<"┓";
-            }
-            else if(i==high && j==0){
-                    cout<<"┗";
-            }
-            else if(i==high && j==length){
-                    cout<<"┛";
-            }
-            else if (j==0 || j==length){
-                    cout << "┃";
-            }
-            else if (i==0||i==high){
-                    cout<<"━";
+void printEndMenu(){
+cout << R"(
 
-            }
-            else if (i==7 && j==18){
-                    cout<<"G";
-            }
-            else if (i==7 && j==19){
-                    cout<<"A";
-            }
-            else if (i==7 && j==20){
-                    cout<<"M";
-            }
-            else if (i==7 && j==21){
-                    cout<<"E";
-            }
-            else if (i==7 && j==23){
-                    cout<<"O";
-            }
-            else if (i==7 && j==24){
-                    cout<<"V";
-            }
-            else if (i==7 && j==25){
-                    cout<<"E";
-            }
-            else if (i==7 && j==26){
-                    cout<<"R";
-            }
-            else{
-                    cout<<" ";
-            }
-        }
-        cout<<endl;
-    
-    }
+  ▄████  ▄▄▄       ███▄ ▄███▓▓█████     ▒█████   ██▒   █▓▓█████  ██▀███  
+ ██▒ ▀█▒▒████▄    ▓██▒▀█▀ ██▒▓█   ▀    ▒██▒  ██▒▓██░   █▒▓█   ▀ ▓██ ▒ ██▒
+▒██░▄▄▄░▒██  ▀█▄  ▓██    ▓██░▒███      ▒██░  ██▒ ▓██  █▒░▒███   ▓██ ░▄█ ▒
+░▓█  ██▓░██▄▄▄▄██ ▒██    ▒██ ▒▓█  ▄    ▒██   ██░  ▒██ █░░▒▓█  ▄ ▒██▀▀█▄  
+░▒▓███▀▒ ▓█   ▓██▒▒██▒   ░██▒░▒████▒   ░ ████▓▒░   ▒▀█░  ░▒████▒░██▓ ▒██▒
+ ░▒   ▒  ▒▒   ▓▒█░░ ▒░   ░  ░░░ ▒░ ░   ░ ▒░▒░▒░    ░ ▐░  ░░ ▒░ ░░ ▒▓ ░▒▓░
+  ░   ░   ▒   ▒▒ ░░  ░      ░ ░ ░  ░     ░ ▒ ▒░    ░ ░░   ░ ░  ░  ░▒ ░ ▒░
+░ ░   ░   ░   ▒   ░      ░      ░      ░ ░ ░ ▒       ░░     ░     ░░   ░ 
+      ░       ░  ░       ░      ░  ░       ░ ░        ░     ░  ░   ░     
+                                                     ░                   
+
+
+ ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+ ┃    Press 's' to start. Press 'e' to end.   ┃
+ ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+    )";
+cout<<"Please widen the window until the ui layout is correct."<<endl;
 }
--- print_victory_menu.cpp
+++ print_victory_menu.cpp
@@ -1,61 +1,23 @@
 #include <iostream>
+
 using namespace std;
 
-void printVictoryMenu(int high,int length){
-        high = 15;
-        length = 45;
-    for (int i = 0; i<=high; i++){
-        for(int j=0;j<=length;j++){
-            if (i==0 && j==0){
-                    cout<<"┏";
-            }
-            else if(i==0 && j==length){
-                    cout<<"┓";
-            }
-            else if(i==high && j==0){
-                    cout<<"┗";
-            }
-            else if(i==high && j==length){
-                    cout<<"┛";
-            }
-            else if (j==0 || j==length){
-                    cout << "┃";
-            }
-            else if (i==0||i==high){
-                    cout<<"━";
+void loadStart(){
+cout << R"(
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃        __     __             ____   _____   U  ___ u   ____     __   __          ┃
+┃        \ \   /"/u  ___    U /"___| |_ " _|   \/"_ \/U |  _"\ u  \ \ / /          ┃
+┃        \ \ / //  |_"_|   \| | u     | |     | | | | \| |_) |/   \ V /            ┃
+┃        /\ V /_,-. | |     | |/__   /| |\.-,_| |_| |  |  _ <    U_|"|_u           ┃
+┃        U  \_/-(_/U/| |\u    \____| u |_|U \_)-\___/   |_| \_\     |_|            ┃
+┃        //   .-,_|___|_,-._// \\  _// \\_     \\     //   \\_.-,//|(_             ┃
+┃        (__)   \_)-' '-(_/(__)(__)(__) (__)   (__)   (__)  (__)\_) (__)           ┃
+┃                                                                                  ┃
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 
-            }
-            else if (i==7 && j==18){
-                    cout<<"V";
-            }
-            else if (i==7 && j==19){
-                    cout<<"I";
-            }
-            else if (i==7 && j==20){
-                    cout<<"C";
-            }
-            else if (i==7 && j==21){
-                    cout<<"T";
-            }
-            else if (i==7 && j==22){
-                    cout<<"O";
-            }
-            else if (i==7 && j==23){
-                    cout<<"R";
-            }
-            else if (i==7 && j==24){
-                    cout<<"Y";
-            }
-            else if (i==7 && j==25){
-                    cout<<"!";
-            }
-            
-            else{
-                    cout<<" ";
-            }
-        }
-        cout<<endl;
-    
-    }   
+ ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+ ┃    Press 's' to start. Press 'e' to end.   ┃
+ ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+    )";
+cout<<"Please widen the window until the ui layout is correct."<<endl;
 }
-
