03c86ecf18e603c4874a9c0962f18c671a80e7b4
Wang-YI-fu <u3609815@connect.hku.hk>
2023-11-26 22:42:54 +0800

    print shopmenu and pausemenu

---
+++ print_pause_menu.h
@@ -0,0 +1,5 @@
+#ifndef PRINT_PAUSE_MENU_H
+#define PRINT_PAUSE_MENU_H
+//read function print_pause_menu from print_pause_menu.cpp
+void print_pause_menu();
+#endif
---
+++ shopmenu.h
@@ -0,0 +1,5 @@
+#ifndef SHOPMENU_H
+#define SHOPMENU_H
+//read function shopmenu from shopmenu.cpp
+void shopmenu();
+#endif
