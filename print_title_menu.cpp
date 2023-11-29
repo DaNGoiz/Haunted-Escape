40c1c1a99943ac087194ded397f837a56b5f2734
Wang Yifu <145696164+Wang-YI-fu@users.noreply.github.com>
2023-11-29 15:07:29 +0800

    ADD comments

--- print_title_menu.cpp
+++ print_title_menu.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 using namespace std;
-
+//print the title of our game
 void printTitleMenu(){
 cout<<" ██░ ██  ▄▄▄       █    ██  ███▄    █ ▄▄▄█████▓▓█████ ▓█████▄    ▓█████   ██████  ▄████▄   ▄▄▄       ██▓███  ▓█████ "<<endl;
 cout<<"▓██░ ██▒▒████▄     ██  ▓██▒ ██ ▀█   █ ▓  ██▒ ▓▒▓█   ▀ ▒██▀ ██▌   ▓█   ▀ ▒██    ▒ ▒██▀ ▀█  ▒████▄    ▓██░  ██▒▓█   ▀ "<<endl;
